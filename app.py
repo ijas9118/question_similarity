2470171ec6ba6267f3acbd6b01028a5e4a1e6266
Ijas Ahammed <ahammedijas.official@gmail.com>
2024-04-05 16:52:10 +0530

    Api done

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from flask import Flask, request, jsonify
 from sentence_transformers import SentenceTransformer, util
 import os
+
+app = Flask(__name__)
 
 model_name = "all-MiniLM-L6-v2"
 model_path = os.path.join("models", model_name)
@@ -21,6 +24,18 @@ questions = [
 ]
 
 questions_embeddings = model.encode(questions)
-input_question_embedding = model.encode("How do I learn Python?")
 
-print(util.semantic_search(questions_embeddings, input_question_embedding))
+
+@app.route('/similar_question', methods=['POST'])
+def get_similar_question():
+    data = request.get_json()
+    input_question = data['question']
+    input_question_embedding = model.encode(input_question)
+    similar_questions = util.semantic_search(input_question_embedding, questions_embeddings, top_k=1)
+    similar_question_index = similar_questions[0][0]['corpus_id']
+    selected_question = questions[similar_question_index]
+    return jsonify({"similar_question": selected_question})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
